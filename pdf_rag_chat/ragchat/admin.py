9cb3565e89f878ab795f7030e47aeef6a49d9719
Ankita-Tode <83173391+Ankita-Tode@users.noreply.github.com>
2025-08-14 13:31:47 +0530

    Update admin.py

--- pdf_rag_chat/ragchat/admin.py
+++ pdf_rag_chat/ragchat/admin.py
@@ -1,1 +1,20 @@
 
+
+from django.contrib import admin
+from django.contrib import admin
+from .models import Document, ChatSession, ChatMessage
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "num_pages", "created_at")
+
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "document", "created_at")
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "session", "role", "created_at")
+    list_filter = ("role",)
+
+# Register your models here.
